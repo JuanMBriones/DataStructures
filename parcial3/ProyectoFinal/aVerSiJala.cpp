ecb3b4d98f355779a2fdfe3a82cd1715b5d3bfb7
Juan Briones <JuanMBriones@outlook.com>
2019-11-25 10:08:27 -0700

    A ver

--- parcial3/ProyectoFinal/aVerSiJala.cpp
+++ parcial3/ProyectoFinal/aVerSiJala.cpp
@@ -1,247 +1,482 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
-
-enum Color {Rojo, Negro};
-
 struct Node
 {
-	int dato;
-	bool color;
-	Node *izquierda, *derecha, *padre;
-
-	Node(int dato)
-	{
-	this->dato = dato;
-	izquierda = derecha = padre = NULL;
-	this->color = Rojo;
-	}
+  int data;
+  Node *parent;
+  Node *left;
+  Node *right;
+  int color;
 };
-
-string traductor(int n) {
-  if(n==0) {
-    return "R";
-  }
-  if(n==1) {
-    return "N";
-  }
-}
-
-class RBArbol
+typedef Node *NodePtr;
+class RedBlackTree
 {
 private:
-	Node *raiz;
-protected:
-	void rotarizquierda(Node *&, Node *&);
-	void rotarderecha(Node *&, Node *&);
-	void fixViolation(Node *&, Node *&);
+  NodePtr root;
+  NodePtr TNULL;
+  void initializeNULLNode(NodePtr node, NodePtr parent)
+  {
+    node->data = 0;
+    node->parent = parent;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->color = 0;
+  }
+  void preOrderHelper(NodePtr node)
+  {
+    if (node != TNULL)
+    {
+      cout << node->data << " ";
+      preOrderHelper(node->left);
+      preOrderHelper(node->right);
+    }
+  }
+  void inOrderHelper(NodePtr node)
+  {
+    if (node != TNULL)
+    {
+      inOrderHelper(node->left);
+      cout << node->data << " ";
+      inOrderHelper(node->right);
+    }
+  }
+  void postOrderHelper(NodePtr node)
+  {
+    if (node != TNULL)
+    {
+      postOrderHelper(node->left);
+      postOrderHelper(node->right);
+      cout << node->data << " ";
+    }
+  }
+  NodePtr searchTreeHelper(NodePtr node, int key)
+  {
+    if (node == TNULL || key == node->data)
+    {
+      return node;
+    }
+    if (key < node->data)
+    {
+      return searchTreeHelper(node->left, key);
+    }
+    return searchTreeHelper(node->right, key);
+  }
+  void deleteFix(NodePtr x)
+  {
+    NodePtr s;
+    while (x != root && x->color == 0)
+    {
+      if (x == x->parent->left)
+      {
+        s = x->parent->right;
+        if (s->color == 1)
+        {
+          s->color = 0;
+          x->parent->color = 1;
+          leftRotate(x->parent);
+          s = x->parent->right;
+        }
+        if (s->left->color == 0 && s->right->color == 0)
+        {
+          s->color = 1;
+          x = x->parent;
+        }
+        else
+        {
+          if (s->right->color == 0)
+          {
+            s->left->color = 0;
+            s->color = 1;
+            rightRotate(s);
+            s = x->parent->right;
+          }
+          s->color = x->parent->color;
+          x->parent->color = 0;
+          s->right->color = 0;
+          leftRotate(x->parent);
+          x = root;
+        }
+      }
+      else
+      {
+        s = x->parent->left;
+        if (s->color == 1)
+        {
+          s->color = 0;
+          x->parent->color = 1;
+          rightRotate(x->parent);
+          s = x->parent->left;
+        }
+        if (s->right->color == 0 && s->right->color == 0)
+        {
+          s->color = 1;
+          x = x->parent;
+        }
+        else
+        {
+          if (s->left->color == 0)
+          {
+            s->right->color = 0;
+            s->color = 1;
+            leftRotate(s);
+            s = x->parent->left;
+          }
+          s->color = x->parent->color;
+          x->parent->color = 0;
+          s->left->color = 0;
+          rightRotate(x->parent);
+          x = root;
+        }
+      }
+    }
+    x->color = 0;
+  }
+  void rbTransplant(NodePtr u, NodePtr v)
+  {
+    if (u->parent == nullptr)
+    {
+      root = v;
+    }
+    else if (u == u->parent->left)
+    {
+      u->parent->left = v;
+    }
+    else
+    {
+      u->parent->right = v;
+    }
+    v->parent = u->parent;
+  }
+  void deleteNodeHelper(NodePtr node, int key)
+  {
+    NodePtr z = TNULL;
+    NodePtr x, y;
+    while (node != TNULL)
+    {
+      if (node->data == key)
+      {
+        z = node;
+      }
+      if (node->data <= key)
+      {
+        node = node->right;
+      }
+      else
+      {
+        node = node->left;
+      }
+    }
+    if (z == TNULL)
+    {
+      cout << "Key not found in the tree" << endl;
+      return;
+    }
+    y = z;
+    int y_original_color = y->color;
+    if (z->left == TNULL)
+    {
+      x = z->right;
+      rbTransplant(z, z->right);
+    }
+    else if (z->right == TNULL)
+    {
+      x = z->left;
+      rbTransplant(z, z->left);
+    }
+    else
+    {
+      y = minimum(z->right);
+      y_original_color = y->color;
+      x = y->right;
+      if (y->parent == z)
+      {
+        x->parent = y;
+      }
+      else
+      {
+        rbTransplant(y, y->right);
+        y->right = z->right;
+        y->right->parent = y;
+      }
+      rbTransplant(z, y);
+      y->left = z->left;
+      y->left->parent = y;
+      y->color = z->color;
+    }
+    delete z;
+    if (y_original_color == 0)
+    {
+      deleteFix(x);
+    }
+  }
+  void insertFix(NodePtr k)
+  {
+    NodePtr u;
+    while (k->parent->color == 1)
+    {
+      if (k->parent == k->parent->parent->right)
+      {
+        u = k->parent->parent->left;
+        if (u->color == 1)
+        {
+          u->color = 0;
+          k->parent->color = 0;
+          k->parent->parent->color = 1;
+          k = k->parent->parent;
+        }
+        else
+        {
+          if (k == k->parent->left)
+          {
+            k = k->parent;
+            rightRotate(k);
+          }
+          k->parent->color = 0;
+          k->parent->parent->color = 1;
+          leftRotate(k->parent->parent);
+        }
+      }
+      else
+      {
+        u = k->parent->parent->right;
+        if (u->color == 1)
+        {
+          u->color = 0;
+          k->parent->color = 0;
+          k->parent->parent->color = 1;
+          k = k->parent->parent;
+        }
+        else
+        {
+          if (k == k->parent->right)
+          {
+            k = k->parent;
+            leftRotate(k);
+          }
+          k->parent->color = 0;
+          k->parent->parent->color = 1;
+          rightRotate(k->parent->parent);
+        }
+      }
+      if (k == root)
+      {
+        break;
+      }
+    }
+    root->color = 0;
+  }
+  void printHelper(NodePtr root, string indent, bool last)
+  {
+    if (root != TNULL)
+    {
+      cout << indent;
+      if (last)
+      {
+        cout << "R----";
+        indent += "   ";
+      }
+      else
+      {
+        cout << "L----";
+        indent += "|  ";
+      }
+      string sColor = root->color ? "RED" : "BLACK";
+      cout << root->data << "(" << sColor << ")" << endl;
+      printHelper(root->left, indent, false);
+      printHelper(root->right, indent, true);
+    }
+  }
 public:
-	RBArbol() { raiz = NULL; }
-	void insertar(const int &n);
-	void inorder();
-	void levelOrder();
+  RedBlackTree()
+  {
+    TNULL = new Node;
+    TNULL->color = 0;
+    TNULL->left = nullptr;
+    TNULL->right = nullptr;
+    root = TNULL;
+  }
+  void preorder()
+  {
+    preOrderHelper(this->root);
+  }
+  void inorder()
+  {
+    inOrderHelper(this->root);
+  }
+  void postorder()
+  {
+    postOrderHelper(this->root);
+  }
+  NodePtr searchTree(int k)
+  {
+    return searchTreeHelper(this->root, k);
+  }
+  NodePtr minimum(NodePtr node)
+  {
+    while (node->left != TNULL)
+    {
+      node = node->left;
+    }
+    return node;
+  }
+  NodePtr maximum(NodePtr node)
+  {
+    while (node->right != TNULL)
+    {
+      node = node->right;
+    }
+    return node;
+  }
+  NodePtr successor(NodePtr x)
+  {
+    if (x->right != TNULL)
+    {
+      return minimum(x->right);
+    }
+    NodePtr y = x->parent;
+    while (y != TNULL && x == y->right)
+    {
+      x = y;
+      y = y->parent;
+    }
+    return y;
+  }
+  NodePtr predecessor(NodePtr x)
+  {
+    if (x->left != TNULL)
+    {
+      return maximum(x->left);
+    }
+    NodePtr y = x->parent;
+    while (y != TNULL && x == y->left)
+    {
+      x = y;
+      y = y->parent;
+    }
+    return y;
+  }
+  void leftRotate(NodePtr x)
+  {
+    NodePtr y = x->right;
+    x->right = y->left;
+    if (y->left != TNULL)
+    {
+      y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == nullptr)
+    {
+      this->root = y;
+    }
+    else if (x == x->parent->left)
+    {
+      x->parent->left = y;
+    }
+    else
+    {
+      x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+  }
+  void rightRotate(NodePtr x)
+  {
+    NodePtr y = x->left;
+    x->left = y->right;
+    if (y->right != TNULL)
+    {
+      y->right->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == nullptr)
+    {
+      this->root = y;
+    }
+    else if (x == x->parent->right)
+    {
+      x->parent->right = y;
+    }
+    else
+    {
+      x->parent->left = y;
+    }
+    y->right = x;
+    x->parent = y;
+  }
+  void insert(int key)
+  {
+    NodePtr node = new Node;
+    node->parent = nullptr;
+    node->data = key;
+    node->left = TNULL;
+    node->right = TNULL;
+    node->color = 1;
+    NodePtr y = nullptr;
+    NodePtr x = this->root;
+    while (x != TNULL)
+    {
+      y = x;
+      if (node->data < x->data)
+      {
+        x = x->left;
+      }
+      else
+      {
+        x = x->right;
+      }
+    }
+    node->parent = y;
+    if (y == nullptr)
+    {
+      root = node;
+    }
+    else if (node->data < y->data)
+    {
+      y->left = node;
+    }
+    else
+    {
+      y->right = node;
+    }
+    if (node->parent == nullptr)
+    {
+      node->color = 0;
+      return;
+    }
+    if (node->parent->parent == nullptr)
+    {
+      return;
+    }
+    insertFix(node);
+  }
+  NodePtr getRoot()
+  {
+    return this->root;
+  }
+  void deleteNode(int data)
+  {
+    deleteNodeHelper(this->root, data);
+  }
+  void printTree()
+  {
+    if (root)
+    {
+      printHelper(this->root, "", true);
+    }
+  }
 };
-
-void inorderHelper(Node *raiz)
-{
-	if (raiz == NULL)
-		return;
-
-	inorderHelper(raiz->izquierda);
-	cout << raiz->dato << traductor(raiz->color) << " ";
-	inorderHelper(raiz->derecha);
-}
-
-Node* BSTinsertar(Node* raiz, Node *pt)
-{
-	if (raiz == NULL)
-	return pt;
-
-	if (pt->dato < raiz->dato)
-	{
-		raiz->izquierda = BSTinsertar(raiz->izquierda, pt);
-		raiz->izquierda->padre = raiz;
-	}
-	else if (pt->dato > raiz->dato)
-	{
-		raiz->derecha = BSTinsertar(raiz->derecha, pt);
-		raiz->derecha->padre = raiz;
-	}
-
-	return raiz;
-}
-
-void levelOrderHelper(Node *raiz)
-{
-	if (raiz == NULL)
-		return;
-
-	queue<Node *> q;
-	q.push(raiz);
-
-	while (!q.empty())
-	{
-		Node *temp = q.front();
-		cout << temp->dato << traductor(temp->color) << " ";
-		q.pop();
-
-		if (temp->izquierda != NULL)
-			q.push(temp->izquierda);
-
-		if (temp->derecha != NULL)
-			q.push(temp->derecha);
-	}
-}
-
-void RBArbol::rotarizquierda(Node *&raiz, Node *&pt)
-{
-	Node *pt_derecha = pt->derecha;
-
-	pt->derecha = pt_derecha->izquierda;
-
-	if (pt->derecha != NULL)
-		pt->derecha->padre = pt;
-
-	pt_derecha->padre = pt->padre;
-
-	if (pt->padre == NULL)
-		raiz = pt_derecha;
-
-	else if (pt == pt->padre->izquierda)
-		pt->padre->izquierda = pt_derecha;
-
-	else
-		pt->padre->derecha = pt_derecha;
-
-	pt_derecha->izquierda = pt;
-	pt->padre = pt_derecha;
-}
-
-void RBArbol::rotarderecha(Node *&raiz, Node *&pt)
-{
-	Node *pt_izquierda = pt->izquierda;
-
-	pt->izquierda = pt_izquierda->derecha;
-
-	if (pt->izquierda != NULL)
-		pt->izquierda->padre = pt;
-
-	pt_izquierda->padre = pt->padre;
-
-	if (pt->padre == NULL)
-		raiz = pt_izquierda;
-
-	else if (pt == pt->padre->izquierda)
-		pt->padre->izquierda = pt_izquierda;
-
-	else
-		pt->padre->derecha = pt_izquierda;
-
-	pt_izquierda->derecha = pt;
-	pt->padre = pt_izquierda;
-}
-
-void RBArbol::fixViolation(Node *&raiz, Node *&pt)
-{
-	Node *padre_pt = NULL;
-	Node *grande_padre_pt = NULL;
-
-	while ((pt != raiz) && (pt->color != Negro) &&
-		(pt->padre->color == Rojo))
-	{
-
-		padre_pt = pt->padre;
-		grande_padre_pt = pt->padre->padre;
-
-		if (padre_pt == grande_padre_pt->izquierda)
-		{
-
-			Node *uncle_pt = grande_padre_pt->derecha;
-
-			if (uncle_pt != NULL && uncle_pt->color == Rojo)
-			{
-				grande_padre_pt->color = Rojo;
-				padre_pt->color = Negro;
-				uncle_pt->color = Negro;
-				pt = grande_padre_pt;
-			}
-
-			else
-			{
-
-				if (pt == padre_pt->derecha)
-				{
-					rotarizquierda(raiz, padre_pt);
-					pt = padre_pt;
-					padre_pt = pt->padre;
-				}
-
-				rotarderecha(raiz, grande_padre_pt);
-				swap(padre_pt->color, grande_padre_pt->color);
-				pt = padre_pt;
-			}
-		}
-
-		else
-		{
-			Node *uncle_pt = grande_padre_pt->izquierda;
-
-			if ((uncle_pt != NULL) && (uncle_pt->color == Rojo))
-			{
-				grande_padre_pt->color = Rojo;
-				padre_pt->color = Negro;
-				uncle_pt->color = Negro;
-				pt = grande_padre_pt;
-			}
-			else
-			{
-
-				if (pt == padre_pt->izquierda)
-				{
-					rotarderecha(raiz, padre_pt);
-					pt = padre_pt;
-					padre_pt = pt->padre;
-				}
-
-				rotarizquierda(raiz, grande_padre_pt);
-				swap(padre_pt->color, grande_padre_pt->color);
-				pt = padre_pt;
-			}
-		}
-	}
-
-	raiz->color = Negro;
-}
-
-void RBArbol::insertar(const int &dato)
-{
-	Node *pt = new Node(dato);
-
-	raiz = BSTinsertar(raiz, pt);
-
-	fixViolation(raiz, pt);
-}
-
-void RBArbol::inorder()	 { inorderHelper(raiz);}
-void RBArbol::levelOrder() { levelOrderHelper(raiz); }
-
 int main()
 {
-	RBArbol Arbol;
-
-	Arbol.insertar(7);
-	Arbol.insertar(6);
-	Arbol.insertar(5);
-	Arbol.insertar(4);
-	Arbol.insertar(3);
-	Arbol.insertar(2);
-	Arbol.insertar(1);
-
-	cout << "Inoder Traversal of Created Arbol\n";
-	Arbol.inorder();
-
-	cout << "\n\nLevel Order Traversal of Created Arbol\n";
-	Arbol.levelOrder();
-
-	return 0;
+  RedBlackTree bst;
+  bst.insert(55);
+  bst.insert(40);
+  bst.insert(65);
+  bst.insert(60);
+  bst.insert(75);
+  bst.insert(57);
+  bst.printTree();
+  cout << endl
+     << "After deleting" << endl;
+  bst.deleteNode(40);
+  bst.printTree();
 }
