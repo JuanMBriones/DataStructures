0539f61fd7b5a0e8a5d61a22893388055b0433bb
Juan Briones <JuanMBriones@outlook.com>
2019-11-25 21:14:55 -0700

    Final

--- dudas/Python3/aVerJeje/prob3.py
+++
@@ -1,9 +0,0 @@
-from sympy import *
-from mpmath import *
-
-x, y = symbols("x y")
-print('#')
-#f = ((mp.e)**(-(x**2)-(y**2)))
-#print(integrate(f, (x, -oo, oo), (y, -oo, oo)).principal_value())
-print(quad(lambda x,y: (((mp.e)**(-(x**2)-(y**2)))), [-inf, inf], [-inf, inf]))
-print('#')
